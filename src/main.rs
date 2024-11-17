1f722ec3c53456e09cc8a4c32fca343994234f1b
Baye Dieng <bayedieng98@gmail.com>
2024-11-17 19:30:00 +0000

    add udp headers

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ impl EthernetHeader {
 #[derive(Debug)]
 struct IpV4Header {
     version: u8,
-    packet_length: u16,
+    pub header_length: u8,
     protocol: u8,
     checksum: u16,
     source_address: [u8; 4],
@@ -54,18 +54,44 @@ impl IpV4Header {
         // The IP version is in the first 4 bits but considering network order is big endian,
         // we get the "last" 4 bits instead
         let version = payload[0] >> 4;
-        let packet_length = u16::from_be_bytes([payload[2], payload[3]]);
+        let header_length = payload[0] & 0xf;
         let protocol = payload[9];
         let checksum = u16::from_be_bytes([payload[10], payload[11]]);
         let source_address: [u8; 4] = payload[12..16].try_into().unwrap();
         let destination_address = payload[16..20].try_into().unwrap();
         Self {
             version,
-            packet_length,
+            header_length,
             protocol,
             checksum,
             source_address,
             destination_address,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct UdpHeader {
+    source_port: u16,
+    destination_port: u16,
+    pub length: u16,
+    checksum: u16,
+    data: Vec<u8>,
+}
+
+impl UdpHeader {
+    pub fn from_ethernet_payload(payload: &[u8]) -> Self {
+        let source_port = u16::from_be_bytes([payload[0], payload[1]]);
+        let destination_port = u16::from_be_bytes([payload[2], payload[3]]);
+        let length = u16::from_be_bytes([payload[4], payload[5]]);
+        let checksum = u16::from_be_bytes([payload[6], payload[7]]);
+        let data = payload[8..].to_vec();
+        Self {
+            source_port,
+            destination_port,
+            length,
+            checksum,
+            data,
         }
     }
 }
@@ -100,6 +126,7 @@ fn main() {
 
         let eth_header = EthernetHeader::from_bytes(buffer[4..packet_length].try_into().unwrap());
         let ip_header = IpV4Header::from_ethernet_payload(&eth_header.payload);
+
         println!("{eth_header:x?}\n{ip_header:?}\nFull Data Length: {packet_length}");
     }
 }
